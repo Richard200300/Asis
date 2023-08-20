5a19c02b9ec0ab9fb38e366ec1fe2eecb525ebdf
Owolabi Precious <owo.pre.eno@gmail.com>
2023-08-20 10:56:27 +0100

    refactored fetching data for category and product

--- Frontend/src/components/specialcategory.jsx
+++ Frontend/src/components/specialcategory.jsx
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from "react";
-import useFetch from "./useFetch.jsx";
 import Products from "./products.jsx";
 import { Link } from "react-router-dom";
 import left_button from "../assets/icons/left_button.svg";
 import right_button from "../assets/icons/right_button.svg";
 import up_arrow from "../assets/icons/up_arrow.svg";
 import Loading from "./loading.jsx";
+import axios from "axios";
 
 const SpecialCategory = ({ name, category, except }) => {
-  let { data } = useFetch(
-    `${import.meta.env.VITE_API_URL}products/?limit=10&category=${
-      category || ""
-    }`,
-  );
-  useEffect(() => {
-    if (data?.products?.length == 0) {
-      const { newData } = useFetch(
-        `${import.meta.env.VITE_API_URL}products/?limit=10`,
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleFetchProducts = async () => {
+    setIsLoading(true);
+    try {
+      let { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}products/?limit=10&category=${
+          category || ""
+        }`,
       );
-      data = newData;
+      if (data.products.length == 1) {
+        let { data } = await axios.get(
+          `${import.meta.env.VITE_API_URL}products/?limit=10`,
+        );
+        setProducts(data.products);
+        return setIsLoading(false);
+      }
+      setProducts(data.products);
+      return setIsLoading(false);
+    } catch (error) {
+      console.log(error);
     }
-  }, [data]);
+  };
+  useEffect(() => {
+    handleFetchProducts();
+  }, []);
   const [scrollPosition, setScrollPosition] = useState(0);
 
   const handleScroll = (increment) => {
@@ -37,63 +51,61 @@ const SpecialCategory = ({ name, category, except }) => {
       container.scrollLeft = newPosition;
     }
   };
-
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <section>
-      {data ? (
-        <section className="relative my-14 flex w-full items-start justify-between border-t border-asisDark pt-12 uppercase">
-          <article className="w-56 text-right text-4xl font-semibold">
-            <p> / {name}</p>
-            <div className="mt-5 w-56 ">
-              <div className="ml-auto flex w-24  items-center justify-between">
-                <img
-                  src={left_button}
-                  alt="left_button"
-                  className="w-8 cursor-pointer"
-                  onClick={() => handleScroll(-305)}
-                />
-                <img
-                  src={right_button}
-                  alt="right_button"
-                  className="w-8 cursor-pointer"
-                  onClick={() => handleScroll(305)}
-                />
-              </div>
+      <section className="relative my-14 flex w-full items-start justify-between border-t border-asisDark pt-12 uppercase">
+        <article className="w-56 text-right text-4xl font-semibold">
+          <p> / {name}</p>
+          <div className="mt-5 w-56 ">
+            <div className="ml-auto flex w-24  items-center justify-between">
+              <img
+                src={left_button}
+                alt="left_button"
+                className="w-8 cursor-pointer"
+                onClick={() => handleScroll(-305)}
+              />
+              <img
+                src={right_button}
+                alt="right_button"
+                className="w-8 cursor-pointer"
+                onClick={() => handleScroll(305)}
+              />
             </div>
-            <div className="absolute bottom-0 w-56">
-              <Link to={`/shop`}>
-                <div className=" ml-auto flex h-8 w-32 cursor-pointer items-center justify-center gap-2 border border-black px-5 uppercase">
-                  <p className="text-xs font-medium">view all</p>
-                  <img src={up_arrow} alt="up_arrow" />
-                </div>
-              </Link>
-            </div>
-          </article>
-          <article className="scroll-container scroll-snap-x mandatory scrollbar-width-thin scrollbar-thumb-gray-500 w-[70%] overflow-x-scroll scroll-smooth pb-5 transition-all duration-300">
-            {data && (
-              <div className="flex gap-10 pr-4">
-                {data.products.map((data) => {
-                  if (data._id === except) return null;
-                  return (
-                    <div key={data._id}>
-                      <Link to={`/product/${data._id}`}>
-                        <Products
-                          name={data.name}
-                          price={data.price}
-                          collaborations={data.collaborations}
-                          images={data.images}
-                        />
-                      </Link>
-                    </div>
-                  );
-                })}
+          </div>
+          <div className="absolute bottom-0 w-56">
+            <Link to={`/shop`}>
+              <div className=" ml-auto flex h-8 w-32 cursor-pointer items-center justify-center gap-2 border border-black px-5 uppercase">
+                <p className="text-xs font-medium">view all</p>
+                <img src={up_arrow} alt="up_arrow" />
               </div>
-            )}
-          </article>
-        </section>
-      ) : (
-        <Loading />
-      )}
+            </Link>
+          </div>
+        </article>
+        <article className="scroll-container scroll-snap-x mandatory scrollbar-width-thin scrollbar-thumb-gray-500 w-[70%] overflow-x-scroll scroll-smooth pb-5 transition-all duration-300">
+          {products.length > 0 && (
+            <div className="flex gap-10 pr-4">
+              {products.map((data) => {
+                if (data._id === except) return null;
+                return (
+                  <div key={data._id}>
+                    <Link to={`/product/${data._id}`}>
+                      <Products
+                        name={data.name}
+                        price={data.price}
+                        collaborations={data.collaborations}
+                        images={data.images}
+                      />
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </article>
+      </section>
     </section>
   );
 };
--- Frontend/src/product/page.jsx
+++ Frontend/src/product/page.jsx
@@ -1,45 +1,59 @@
 import React, { useState, useEffect } from "react";
-import Axios from "axios";
 import { useParams } from "react-router-dom";
 import Product_detail from "./component/product_detail";
-import Banner from "../components/banner";
-import Cart from "../components/cart";
-import useFetch from "../components/useFetch";
 import SpecialCategory from "../components/specialCategory";
 import Loading from "../components/loading";
+import axios from "axios";
 
 const Page = () => {
   const { id } = useParams();
   const [hideCart, ShowCart] = useState(false);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const apiUrl = `${import.meta.env.VITE_API_URL}products/${id}`;
-  const { data } = useFetch(apiUrl);
+  // const { data } = useFetch(apiUrl);
 
   const name = "you may also like";
+  const handleFetchProducts = async () => {
+    setIsLoading(true);
+    try {
+      let { data } = await axios.get(apiUrl);
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
   useEffect(() => {
+    handleFetchProducts();
+  }, [id]);
+  useEffect(() => {
+    handleFetchProducts();
     window.scrollTo(0, 0);
   }, []);
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div className=" h-full px-8">
-      {data ? (
-        <div>
-          {/* {hideCart && <Cart data={data} hideCart={hideCart} ShowCart={ShowCart} />} */}
+      <div>
+        {/* {hideCart && <Cart data={data} hideCart={hideCart} ShowCart={ShowCart} />} */}
 
-          {data && (
-            <div className="">
-              <Product_detail id={id} data={data} ShowCart={ShowCart} />
-              <SpecialCategory
-                category={data.category}
-                name={name}
-                except={data._id}
-              />
-            </div>
-          )}
-        </div>
-      ) : (
-        <Loading />
-      )}
+        {data && (
+          <div className="">
+            <Product_detail id={id} data={data} ShowCart={ShowCart} />
+            <SpecialCategory
+              category={data.category}
+              name={name}
+              except={data._id}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
